ff19b5177b986ac2770a400ed2f93c7f4f49acac
YaoGalteland <yaojgalteland@Mac.home>
2025-03-26 15:58:54 +0100

    change the proof obj to poseidon2-permutations

--- examples/examples/run_prove_prime_field.sh
+++ examples/examples/run_prove_prime_field.sh
@@ -3,7 +3,7 @@
 # Loop to run the commands 10 times
 for i in $(seq 1 10); do
   # Run the command and redirect output to text files
-  RUSTFLAGS="-Ctarget-cpu=native" cargo run --example prove_prime_field_31 --release --features parallel -- --field koala-bear --objective keccak-f-permutations --log-trace-length 19 --discrete-fourier-transform radix-2-dit-parallel --merkle-hash poseidon-2 > "test-data/data-poseidon2-${i}.txt"
-  RUSTFLAGS="-Ctarget-cpu=native" cargo run --example prove_prime_field_31 --release --features parallel -- --field koala-bear --objective keccak-f-permutations --log-trace-length 19 --discrete-fourier-transform radix-2-dit-parallel --merkle-hash keccak-f > "test-data/data-keccak-f-${i}.txt"
+  RUSTFLAGS="-Ctarget-cpu=native" cargo run --example prove_prime_field_31 --release --features parallel -- --field koala-bear --objective poseidon-2-permutations --log-trace-length 19 --discrete-fourier-transform radix-2-dit-parallel --merkle-hash poseidon-2 > "test-data/data-poseidon2-${i}.txt"
+  RUSTFLAGS="-Ctarget-cpu=native" cargo run --example prove_prime_field_31 --release --features parallel -- --field koala-bear --objective poseidon-2-permutations --log-trace-length 19 --discrete-fourier-transform radix-2-dit-parallel --merkle-hash keccak-f > "test-data/data-keccak-f-${i}.txt"
 done
 
