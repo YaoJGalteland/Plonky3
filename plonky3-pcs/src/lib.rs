74fbaca4a7e05b1fe9fff8564273bff459a8edac
YaoGalteland <yaojgalteland@Mac.home>
2025-04-07 13:30:18 +0200

    test: add avx512 flags

--- plonky3-pcs/src/lib.rs
+++ plonky3-pcs/src/lib.rs
@@ -1,3 +1,18 @@
+#![no_std]
+#![cfg_attr(
+    all(
+        feature = "nightly-features",
+        target_arch = "x86_64",
+        target_feature = "avx512f"
+    ),
+    feature(stdarch_x86_avx512)
+)]
+
+#[cfg(all(
+    feature = "nightly-features",
+    target_arch = "x86_64",
+    target_feature = "avx512f"
+))]
 mod tests_circle;
 //mod tests_fri;
 pub mod utilities;
