1c3a66c4f7148462bd01f6c1fa09a0a0d610403b
YaoGalteland <yaojgalteland@Mac.home>
2025-04-08 22:34:15 +0200

    style: fmt

--- circle/benches/cfft.rs
+++ circle/benches/cfft.rs
@@ -1,15 +1,15 @@
 use criterion::measurement::Measurement;
 use criterion::{BenchmarkGroup, BenchmarkId, Criterion, criterion_group, criterion_main};
+use p3_baby_bear::BabyBear;
 use p3_circle::{CircleDomain, CircleEvaluations};
 use p3_dft::{Radix2Bowers, Radix2Dit, Radix2DitParallel, TwoAdicSubgroupDft};
 use p3_field::TwoAdicField;
+use p3_koala_bear::KoalaBear;
 use p3_matrix::dense::RowMajorMatrix;
 use p3_mersenne_31::Mersenne31;
 use p3_util::pretty_name;
 use rand::distr::{Distribution, StandardUniform};
 use rand::rng;
-use p3_baby_bear::BabyBear;
-use p3_koala_bear::KoalaBear;
 
 fn bench_lde(c: &mut Criterion) {
     let log_n = 18;
