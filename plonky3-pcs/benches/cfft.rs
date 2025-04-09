34811f754eba34f809a17ecef28add2230f5499c
YaoGalteland <yaojgalteland@Mac.home>
2025-04-09 22:21:58 +0200

    see details

--- plonky3-pcs/benches/cfft.rs
+++ plonky3-pcs/benches/cfft.rs
@@ -15,13 +15,13 @@ use rand::rng;
 fn bench_lde_diff_flags(c: &mut Criterion) {
 
     for log_n in 18..22 {
-        for log_w in 1..10 {
+        for log_w in 3..6 {
             let mut g = c.benchmark_group(format!("lde for different flags: log_n={},log_w={}", log_n,log_w));
             g.sample_size(10);
             lde_cfft(&mut g, log_n, log_w);
             lde_twoadic::<KoalaBear, Radix2Dit<_>, _>(&mut g, log_n, log_w);
             lde_twoadic::<KoalaBear, Radix2DitParallel<_>, _>(&mut g, log_n, log_w);
-            lde_twoadic::<KoalaBear, Radix2Bowers, _>(&mut g, log_n, log_w);
+            //lde_twoadic::<KoalaBear, Radix2Bowers, _>(&mut g, log_n, log_w);
         }
     }
 
