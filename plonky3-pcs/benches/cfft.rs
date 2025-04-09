4f2550be7d547d2a0381efe48b158356e7b16ef9
YaoGalteland <yaojgalteland@Mac.home>
2025-04-09 23:05:02 +0200

    see details

--- plonky3-pcs/benches/cfft.rs
+++ plonky3-pcs/benches/cfft.rs
@@ -15,7 +15,7 @@ use rand::rng;
 fn bench_lde_diff_flags(c: &mut Criterion) {
 
     for log_n in 18..22 {
-        for log_w in 6..9 {
+        for log_w in 6..12 {
             let mut g = c.benchmark_group(format!("lde for different flags: log_n={},log_w={}", log_n,log_w));
             g.sample_size(10);
             lde_cfft(&mut g, log_n, log_w);
